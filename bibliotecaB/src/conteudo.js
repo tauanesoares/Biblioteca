5544e9f5dc0f88b8979c786051de6acb9acc97c7
tauane <tauane-soares@hotmail.com>
2024-10-15 09:46:06 -0300

    first commit

---
+++ bibliotecaB/src/conteudo.js
@@ -0,0 +1,27 @@
+const conteudo = {
+    "9cfc4": {
+        titulo: "Código limpo",
+        autor: "Robert C Martin",
+        ano: 2009,
+        genero: ["Design de Software","Programação"],
+        paginas: 425,
+        capa: "https://altabooks.com.br/wp-content/uploads/2021/07/Codigo_Limpo-scaled.jpg",
+        descricao: "mesmo um código ruim pod...",
+        avaliacao: 4.8,
+        capadura: true
+    },
+    "9cfc6": {
+        titulo: "Código limpo",
+        autor: "Robert C Martin",
+        ano: 2009,
+        genero: ["Design de Software","Programação"],
+        paginas: 425,
+        capa: "https://altabooks.com.br/wp-content/uploads/2021/07/Codigo_Limpo-scaled.jpg",
+        descricao: "mesmo um código ruim pod...",
+        avaliacao: 4.8,
+        capadura: true
+    }
+
+}
+
+export default conteudo;
